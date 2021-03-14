227c6a870689ef788c772f904616a080403114b4
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-14 23:58:48 +0000

    Finished task-1

---
+++ task-1/main.py
@@ -0,0 +1,9 @@
+football_managers = {
+    "Manchester United": "Alex Ferguson",
+    "Arsenal": "Arsene Wenger",
+    "Everton": "David Moyes",
+    "Norwich City": "Chris Hughton"
+}
+
+# Print the manager of arsenal:
+print("The manager for the football team arsenal is: " + football_managers['Arsenal'])
