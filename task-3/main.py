60072e24cdf98cd240643d7f493b41a6364d1b4d
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-15 00:04:29 +0000

    Finished task-3

---
+++ task-3/main.py
@@ -0,0 +1,13 @@
+football_managers = {
+    "Manchester United": "Alex Ferguson",
+    "Arsenal": "Arsene Wenger",
+    "Everton": "David Moyes",
+    "Norwich City": "Chris Hughton",
+    "Chelsea": "Rafael Benitez"
+}
+
+keys = []
+for key in football_managers.keys():
+    keys.append(key)
+
+print(keys)
