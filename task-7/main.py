324d208ebf36ca0909673eb649680f02d9d9d377
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-15 00:44:06 +0000

    Finished task-7 and task-8

---
+++ task-7/main.py
@@ -0,0 +1,12 @@
+bond_films = {
+    "2008": ["Daniel Craig", "Quantom of Solace", 8.9, 514.2],
+    "2006": ['Daniel Craig', "Casino Royale", 3.4, 581.5],
+    "2002": ['Pierce Brosnan', "Die Another Day", 16.5, 465.4],
+    "1999": ["Pierce Brosnan", "The World Is Not Enough", 12.4, 439.5]
+}
+
+keys = []
+for key in bond_films.keys():
+    keys.append(key)
+
+print(keys)
