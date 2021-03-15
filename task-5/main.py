db916fa12dadb960a74917ff2a22f442b490cbe0
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-15 00:26:01 +0000

    Finished task-5

---
+++ task-5/main.py
@@ -0,0 +1,7 @@
+bond_films = {
+    "2008": ["Daniel Craig", "Quantom of Solace", 8.9, 514.2],
+    "2006": ['Daniel Craig', "Casino Royale", 3.4, 581.5],
+    "2002": ['Pierce Brosnan', "Die Another Day", 16.5, 465.4]
+}
+
+print(bond_films)
