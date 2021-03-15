742f21f92bd7e2a3635da4363986cac19c0529c0
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-03-15 00:01:34 +0000

    Finished task-2

---
+++ task-2/main.py
@@ -0,0 +1,9 @@
+football_managers = {
+    "Manchester United": "Alex Ferguson",
+    "Arsenal": "Arsene Wenger",
+    "Everton": "David Moyes",
+    "Norwich City": "Chris Hughton",
+    "Chelsea": "Rafael Benitez"
+}
+
+print(football_managers)
